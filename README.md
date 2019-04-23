287ec0b5b679082577bd60e892a24a41019f772b
zedix <lx.vignal@gmail.com>
2019-04-23 14:31:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# @zedix/prettier-config
+
+![version](https://img.shields.io/github/package-json/v/zedix/prettier-config.svg?maxAge=60)
+![tag](https://img.shields.io/github/tag/zedix/prettier-config.svg?maxAge=60)
+
+## Installation
+
+```sh
+$ yarn add --dev zedix/prettier-config.git#v1.x.x
+```
+
+Edit `package.json`:
+
+```json
+{
+  // ...
+  "prettier": "@zedix/prettier-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf', // Default: 'auto'
+  htmlWhitespaceSensitivity: 'css',
+  printWidth: 80,
+  proseWrap: 'never', // Default: 'preserve'
+  semi: true,
+  singleQuote: true, // Default: false
+  tabWidth: 2,
+  trailingComma: 'es5', // Default: 'none'
+  useTabs: false,
+  overrides: [
+    {
+      files: 'package*.json',
+      options: {
+        printWidth: Infinity,
+      },
+    },
+  ],
+};
